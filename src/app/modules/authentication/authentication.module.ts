4b5de97af7a295f4ef22d28e654a92fa7e9905ec
rami9237 <ramibenromdhane2@gmail.com>
2023-10-19 13:11:51 +0200

    login UI update

--- src/app/modules/authentication/authentication.module.ts
+++ src/app/modules/authentication/authentication.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { DividerModule } from 'primeng/divider';
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, PasswordModule, ButtonModule],
+  imports: [
+    CommonModule,
+    PasswordModule,
+    ButtonModule,
+    CheckboxModule,
+    DividerModule,
+  ],
   exports: [LoginComponent],
 })
 export class AuthenticationModule {}
