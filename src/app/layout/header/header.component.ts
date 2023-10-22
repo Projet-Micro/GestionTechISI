29bde73bb9eae55b7e9c6c295346aa502ab9ffa5
rami9237 <ramibenromdhane2@gmail.com>
2023-10-22 14:34:48 +0200

    Login modal UI update and menubar items

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,17 +1,69 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/core/authentication/modal.service';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  constructor(private modalService: ModalService) {}
+export class HeaderComponent implements OnInit {
+  items: MenuItem[] | undefined;
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Projecteur',
+        icon: 'pi pi-video',
+        items: [
+          {
+            label: 'Créer',
+            icon: 'pi pi-fw pi-plus-circle',
+          },
+          {
+            label: 'Supprimer',
+            icon: 'pi pi-fw pi-minus-circle',
+          },
+          {
+            label: 'Afficher liste',
+            icon: 'pi pi-fw pi-bars',
+          },
+        ],
+      },
+      {
+        label: 'Utilisateur',
+        icon: 'pi pi-fw pi-user',
+        items: [
+          {
+            label: 'Créer',
+            icon: 'pi pi-fw pi-user-plus',
+          },
+          {
+            label: 'Supprimer',
+            icon: 'pi pi-fw pi-user-minus',
+          },
+          {
+            label: 'Chercher',
+            icon: 'pi pi-fw pi-users',
+            items: [
+              {
+                label: 'Filter',
+                icon: 'pi pi-fw pi-filter',
+              },
+              {
+                icon: 'pi pi-fw pi-bars',
+                label: 'Afficher liste',
+              },
+            ],
+          },
+        ],
+      },
+    ];
+  }
   get visible() {
     return this.modalService.isVisible;
   }
   showDialog() {
     this.modalService.showDialog();
   }
+  constructor(private modalService: ModalService) {}
 }
