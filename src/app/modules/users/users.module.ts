aed846a7662e149846727cd132087e1d645a74a0
rami9237 <ramibenromdhane2@gmail.com>
2023-11-18 12:04:13 +0100

    initial push on users CRUD

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersDatatableComponent } from './users-datatable/users-datatable.component';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [UsersDatatableComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, TableModule],
   exports: [UsersDatatableComponent],
 })
 export class UsersModule {}
