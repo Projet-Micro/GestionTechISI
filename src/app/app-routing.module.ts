0f017d57da8ff6eea31e2ffe584d2dde5669efc4
rami9237 <ramibenromdhane2@gmail.com>
2023-11-19 14:59:28 +0100

    ignore lint rule and add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from './modules/authentication/authentication.component';
 import { authGuard } from './shared/guards/auth.guard';
 import { DashboardComponent } from './modules/dashboard-main/dashboard/dashboard.component';
+import { UsersComponent } from './modules/users/users.component';
 
 const routes: Routes = [
   // { path: 'home', component: HomeComponent, canActivate: [authGuard] },
   { path: 'auth', component: AuthenticationComponent },
+  { path: 'users', component: UsersComponent, canActivate: [authGuard] },
   { path: '**', component: DashboardComponent, canActivate: [authGuard] },
 ];
 @NgModule({
